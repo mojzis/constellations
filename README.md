09a6530705437071f15a08445d89d45c9de9da6e
Mojzis Stupka <mojzis.stupka@showmax.com>
2020-12-05 21:02:50 +0100

    added thumbnails

---
+++ README.md
@@ -0,0 +1,34 @@
+# Constellations
+
+a simple static website to show the images of constellations as shown in https://stellarium.org when a different culture is selected.
+
+## Todo
+
+- REFACTOR
+- add an about page describing this
+- try parsing & showing the culture descriptions
+- maybe try to organize the source repo a bit ?
+- change deployment - make it run in python directly
+- google, sharing stuff (whew)
+
+- list cultures (also ?) per region
+- some kind of search ? (huh, javascript ... or google / someone else ? might be better for starters )
+- fetch culture list from github, somehow discern those w/out pictures
+- add next / previous to culture page
+- "proper behavior" - alts on images, links ...
+
+## issues
+- gitcdn does a nasty redirect to hash (?) - takes extra 300ms
+
+## NTH
+
+- show constellations that are around similar stars in other cultures
+- overlay with stars (not sure it makes sense though)
+- link to the stellarium web for each constellation (<- that would be excellent)
+- tweetbot to publish 1 constellation daily (?) w/ a link
+- publish thumbs upstream
+- t-shirts :) or something ...
+- FIND MORE ART ?! - Africa ?
+
+## done
+- add thumbnails
--- do.py
+++ do.py
@@ -8,12 +8,46 @@ from jinja2 import Environment, FileSystemLoader
 from slugify import slugify
 import requests
 
+from PIL import Image, UnidentifiedImageError
+
+PUBLIC_DIR = 'public'
+THUMB_DIR = f'{PUBLIC_DIR}/thumbs'
+IMG_DIR = f'{PUBLIC_DIR}/img'
+THUMB_EXT = 'webp'
+IMG_EXT = 'webp'
+THUMB_SIZE = 70
+
+REPO_URL = 'https://raw.githubusercontent.com/Stellarium/stellarium-skycultures/master'
+
 @click.group()
 def main():
     """click"""
     pass
 
-
+def create_thumbnail(culture, orig_file_name, file_name):
+    # todo: check extension
+    thumb_file = f'{THUMB_DIR}/{culture}/{file_name}.{THUMB_EXT}'
+    img_file = f'{IMG_DIR}/{culture}/{file_name}.{IMG_EXT}'
+    # check if thumb exists
+    if os.path.isfile(thumb_file):
+        return
+    else:
+        if not os.path.isfile(img_file):
+            # https://github.com/Stellarium/stellarium-skycultures/blob/master/
+            # arabic/illustrations/Bearer-of-the-Demons-Head.webp?raw=true
+            r = requests.get(
+                f'{REPO_URL}/{culture}/{orig_file_name}?raw=true'
+                , allow_redirects=True)
+            # TODO: fail if r 404
+            i = open(img_file,'wb')
+            i.write(r.content)
+        
+        try:
+            src_img = Image.open(img_file)
+            src_img.thumbnail((THUMB_SIZE,THUMB_SIZE))
+            src_img.save(thumb_file)
+        except UnidentifiedImageError as e:
+            print(e)
 
 @click.command()
 def pub():
@@ -44,11 +78,14 @@ def pub():
                 , allow_redirects=True)
             f = open(data_path,'wb')
             f.write(r.content)
+            # this looks totally broken, is there a better way ? (r.content is binary)
             f = open(data_path,'r')
 
         culture_data = json.load(f)
         f.close()
         os.makedirs(f'public/{culture["slug"]}',exist_ok=True)
+        os.makedirs(f'{THUMB_DIR}/{culture["slug"]}',exist_ok=True)
+        os.makedirs(f'{IMG_DIR}/{culture["slug"]}',exist_ok=True)
         constellations = culture_data['constellations']
         env = Environment(
             loader=FileSystemLoader('templates'),
@@ -57,8 +94,12 @@ def pub():
         prev = 'index'
         for con in constellations:
             image = con.get('image','none')
+            # for now we only create a page when a constellation has an image
             if image == 'none':
                 continue
+            img_path = image['file']
+            img_filename = pathlib.Path(img_path).stem
+            create_thumbnail(culture["slug"], img_path, img_filename)
             # create a page for each constellation
             native = con['common_name'].get('native','none')
             name = con['common_name'].get('english')
@@ -70,7 +111,8 @@ def pub():
                 name = native
             filename = slugify(name)
             const_data.append({
-                        "img" : image['file'],
+                        "img" : img_path,
+                        'thumb_filename': f'{img_filename}.{THUMB_EXT}',
                         'name': name,
                         'trans': trans,
                         'filename' : filename,
@@ -82,6 +124,9 @@ def pub():
         for j in range(0,len(const_data)-1):
             const_data[j]['next'] = const_data[j+1]['filename']
         # const_data[len(const_data)]['next'] = 'index'
+
+        # assign a thumb from the first constellation
+        culture['thumb_filename'] = const_data[0]['thumb_filename']
 
         for const in const_data:
             with open(f'public/{culture["slug"]}/{const["filename"]}.html','w') as s:
